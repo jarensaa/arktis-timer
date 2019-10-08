94903cb55f2a2d6c6f7d117eadc635d9e39125ff
Jens-Andreas Rensaa <jens.rensaa@gmail.com>
2019-10-08 10:39:24 +0200

    Split code in modules and add css grid

--- src/App.js
+++ src/App.js
@@ -1,65 +1,15 @@
 import React, {useState, useEffect} from 'react';
-import './App.css';
+import Countdown from './countdown/Countdown';
+import "./App.css"
 
 var dueDate = new Date(2019, 10, 28, 12);
 
 function App() {
-
-  const [time, setTime] = useState(getTimeToDueDate())
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(getTimeToDueDate())
-    }, 500);
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
+  return(
     <div className="App">
-      <div className="App-box">
-        <a>
-          <a className="big-text">
-            {DateDiff.inDays(time)}
-          </a>
-          <a className="annotation-text">
-            dager
-          </a>
-        </a>
-        <a>
-          <a className="small-text">
-            {DateDiff.inHours(time)}
-          </a>
-          <a className="annotation-text">
-            timer
-          </a>
-        </a>
-        <a>
-          <a className="small-text">
-            {DateDiff.inSeconds(time)}
-          </a>
-          <a className="annotation-text">
-            sekunder
-          </a>
-        </a>
-      </div>
+      <Countdown/>
     </div>
-  );
-}
-
-function getTimeToDueDate() {
-  return dueDate - Date.now();
-}
-
-var DateDiff = {
-  inDays: (date) => {
-      return parseInt(date/(24*3600*1000));
-  },
-  inHours: (date) => {
-    return parseInt(date/(3600*1000));
-  },
-  inSeconds: (date) => {
-    return parseInt((date)/(1000));
-  }
+  )
 }
 
 export default App;
