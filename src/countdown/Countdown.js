e4d57fdc21784d59d11043e4bdf14c338b61d7d6
Jens-Andreas Rensaa <jens.rensaa@gmail.com>
2019-11-28 11:43:05 +0100

    Change countdown date to Jens' end date

--- src/countdown/Countdown.js
+++ src/countdown/Countdown.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Countdown.css";
 
-var dueDate = new Date(2019, 10, 28, 12);
+var dueDate = new Date(2019, 11, 5, 18);
 
 function Countdown() {
   const [time, setTime] = useState(getTimeToDueDate());
